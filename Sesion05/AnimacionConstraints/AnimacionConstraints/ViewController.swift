6cf503ca1ec93418a9dc55c566ae06764a3f33b9
Kenyi Rodriguez <krodriguez@solera.pe>
2020-09-28 08:31:34 -0500

    Se creo el ejercicio de constraints parte 2 y se inicio el proyecto de animación de constratins

---
+++ Sesion05/AnimacionConstraints/AnimacionConstraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimacionConstraints
+//
+//  Created by Kenyi Rodriguez on 9/28/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sesion05/DemoConstraintsParte2/DemoConstraintsParte2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DemoConstraintsParte2
+//
+//  Created by Kenyi Rodriguez on 9/28/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        /*
+        let scrollView = UIScrollView()
+        scrollView.frame //Regresa el tamaño y el origin del componente UI
+        //Si el size del contenido es igual el frame.size del scroll -> no presenta desplazamiento ni horizontal, ni vertical.
+        scrollView.contentSize //Regresa el tamaño del contenido
+        scrollView.contentOffset //Regresa el desplazamiento del contenido en el scroll
+        
+        //de alto el scroll mide 200
+        //de alto el contenido mide 450
+     */
+    }
+}
+
