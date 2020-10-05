6111ece6aa298bccbf0a1ebbaff070f7402f2cc9
Kenyi Rodriguez <krodriguez@solera.pe>
2020-10-05 08:50:48 -0500

    Sesion06 parcial, Ejercio1, Ejercicio2

---
+++ Sesion06/InputConConstraint/InputConConstraint/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  InputConConstraint
+//
+//  Created by Kenyi Rodriguez on 10/5/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var viewContent: UIView!
+    @IBOutlet weak var constraintCenterYViewContent: NSLayoutConstraint!
+    
+    @IBAction func clickBtnCloseKeyboard(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        
+        let keyboardOriginY = keyboardFrame.origin.y
+        let finalPosYViewContent = self.viewContent.frame.origin.y + self.viewContent.frame.height
+        
+        var delta: CGFloat = 0
+        let viewContentSpaceKeyboard: CGFloat = 10
+        
+        if keyboardOriginY < finalPosYViewContent {
+            delta = keyboardOriginY - finalPosYViewContent - viewContentSpaceKeyboard
+        }
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.constraintCenterYViewContent.constant = delta
+            self.view.layoutIfNeeded()
+            
+        }
+        
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        
+        let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.constraintCenterYViewContent.constant = 0
+            self.view.layoutIfNeeded()
+            
+        }
+    }
+
+}
+
