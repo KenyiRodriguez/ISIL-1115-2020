3c2ad55d41338a3efbddc7b90813a7b96be95d65
Kenyi Rodriguez <krodriguez@solera.pe>
2020-10-12 08:03:23 -0500

    sesion07 parcial

---
+++ Sesion07/DemoCustomView/DemoCustomView/ISILView.swift
@@ -0,0 +1,56 @@
+//
+//  ISILView.swift
+//  DemoCustomView
+//
+//  Created by Kenyi Rodriguez on 10/12/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class ISILView: UIView {
+    
+    @IBInspectable var borderColor: UIColor = .black {
+        didSet {
+            self.layer.borderColor = self.borderColor.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = self.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = self.cornerRadius
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor = .black {
+        didSet {
+            self.layer.shadowColor = self.shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize = CGSize(width: 0, height: 0) {
+        didSet {
+            self.layer.shadowOffset = self.shadowOffset
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            self.layer.shadowRadius = self.shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float = 0 {
+        didSet {
+            self.layer.shadowOpacity = self.shadowOpacity
+        }
+    }
+    
+    
+}
